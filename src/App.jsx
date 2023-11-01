045abd0cfc4fee0fe6f6fb6b499879a4ef329d26
Valeria Quintero <valqj.09@gmail.com>
2023-11-01 13:14:44 -0500

    arreglo1

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Card from "./Card";
+import Card from './Components/Card'
 
 
 function App() {
