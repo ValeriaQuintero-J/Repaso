c0bda0f8ad4912eb9b005ce0853b5ac451868993
Valeria Quintero <valqj.09@gmail.com>
2023-11-01 20:55:05 -0500

    Entrega

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,55 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Card from './Components/Card'
+import Datos from './Components/Datos'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const [nombre, setNombre] = useState("")
+  const [pelicula, setPelicula] = useState("")
 
+  const handleSubmit = e => {
+      e.preventDefault()
+
+      const nombreSinEspacios = nombre.trimStart();
+
+        if(nombreSinEspacios.length>2 && pelicula.length>5){
+            console.log(`Nombre: ${nombre}`);
+            console.log(`Pelicula: ${pelicula}`);
+        }else{
+            alert("Por favor chequea que la información sea correcta");
+        }
+  }
   return (
-    <div className="App">
-      <h1>Carga de estudiantes</h1>
-      <form></form>
-      <Card />
-    </div>
+    <>
+    <h2>Peliculas Favoritas</h2>
+    <form onSubmit={handleSubmit}>
+        <label htmlFor='nombre'>Nombre: </label>
+        <input
+            type='text'
+            placeholder='Ingresar Nombre'
+            id='nombre'
+            value={nombre}
+            onChange={e =>setNombre(e.target.value)}
+        />
+
+        <label htmlFor='pelicula'>Pelicula: </label>
+        <input
+            type='text'
+            placeholder='Ingrese su pelicula favorita'
+            id='pelicula'
+            value={pelicula}
+            onChange={e =>setPelicula(e.target.value)}
+        />
+        <button type="submit">Enviar</button>
+
+  </form>
+  <Datos
+    nombre= {nombre}
+    pelicula= {pelicula}
+  />
+  </>
   )
 }
 
---
+++ src/Components/Datos.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Datos = ({nombre, pelicula}) => {
+  return (
+    <>
+      <p>Mi nombre es: {nombre}</p>
+      <p>Mi pelicula Favorita es: {pelicula}</p>
+    </>
+  )
+}
+
+export default Datos
